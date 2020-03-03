0c7ec1057b9b70b2b90eb1a9246fc82b67b2bbf7
Vitor Costa <deividevitor565@gmail.com>
2020-03-03 11:25:11 -0300

    Added task for decode JSON

--- MinionMovies/MovieListController.swift
+++ MinionMovies/MovieListController.swift
@@ -11,21 +11,43 @@ import UIKit
 class MovieListController: UIViewController {
 
     private let moviesImages: [String] = ["jumanji", "reiLeão"]
+    private let session = URLSession.shared
+    private let url = URL(string: "http://localhost:8080/response.json")!
+    private var movies: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            if error == nil {
+                guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode),
+                let mime = response?.mimeType, mime == "application/json" else {
+                        return
+                }
+                
+                do {
+                    if let dataChecked = data {
+                        self.movies = try JSONDecoder().decode([Movie].self, from: dataChecked)
+                    }
+                } catch {
+                    print("JSON error: \(error.localizedDescription)")
+                }
+            } else {
+                debugPrint(error?.localizedDescription)
+            }
+        }
+        task.resume()
     }
-
 }
 
 extension MovieListController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return moviesImages.count
+        return movies.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let movieCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as? MovieCell {
-            guard let image = UIImage(named: moviesImages[indexPath.row]) else {
+            guard let image = UIImage(contentsOfFile: movies[indexPath.row].images.first!) else {
                 return MovieCell()
             }
             movieCell.populate(with: image)
