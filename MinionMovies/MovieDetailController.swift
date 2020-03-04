a8c3ecbbcebbeba2eb1122f66799035a638e6c76
Vitor Costa <deividevitor565@gmail.com>
2020-03-04 17:57:34 -0300

    Added first items of movieDetail scene

---
+++ MinionMovies/MovieDetailController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailController.swift
+//  MinionMovies
+//
+//  Created by Vitor Costa on 04/03/20.
+//  Copyright © 2020 Vitor Costa. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+        
+    }
+}
