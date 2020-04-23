215641c9f4f2bcd9d8b5dc4e18c392a81faad943
Vitor Costa <deividevitor565@gmail.com>
2020-04-23 14:43:13 -0300

    change files to other folder

---
+++ MinionMoviesTests2/MinionMoviesTests2.swift
@@ -0,0 +1,33 @@
+//
+//  MinionMoviesTests2.swift
+//  MinionMoviesTests2
+//
+//  Created by Vitor Costa on 22/04/20.
+//  Copyright © 2020 Vitor Costa. All rights reserved.
+//
+
+import XCTest
+
+class MinionMoviesTests2: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
